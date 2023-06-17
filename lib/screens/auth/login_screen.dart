cd8c3565d058b966cdc9c0597596bd0538e079b2
Luiz-Eduardo-BL <limadudu34@gmail.com>
2023-06-17 19:50:13 -0300

    Autenticação de login com Google e Manual

--- lib/screens/auth/login_screen.dart
+++ lib/screens/auth/login_screen.dart
@@ -1,139 +1,275 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 class LoginScreen extends StatefulWidget {
   @override
-  _LoginScreenState createState() => _LoginScreenState();
+  State<LoginScreen> createState() => _LoginScreenState();
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  final GoogleSignIn googleSignIn = GoogleSignIn();
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+
+  void _loginWithGoogle(BuildContext context) async {
+    try {
+      final GoogleSignInAccount? googleSignInAccount =
+          await googleSignIn.signIn();
+      final GoogleSignInAuthentication googleSignInAuthentication =
+          await googleSignInAccount!.authentication;
+
+      final AuthCredential credential = GoogleAuthProvider.credential(
+        accessToken: googleSignInAuthentication.accessToken,
+        idToken: googleSignInAuthentication.idToken,
+      );
+
+      await _auth.signInWithCredential(credential);
+
+      // Redirecionar para a tela Home
+      Navigator.pushReplacementNamed(context, '/home');
+    } catch (e) {
+      print(e);
+      // Tratar erro de login com Google
+    }
+  }
+
+  void _loginWithEmailAndPassword() async {
+    try {
+      final String username = _usernameController.text;
+      final String password = _passwordController.text;
+
+      await _auth.signInWithEmailAndPassword(
+        email: username,
+        password: password,
+      );
+
+      // Redirecionar para a tela Home
+      Navigator.pushReplacementNamed(context, '/home');
+    } catch (e) {
+      print(e);
+      // Tratar erro de login com email e senha
+    }
+  }
+
+  @override
+  void dispose() {
+    _usernameController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    const baseWidth = 390.0;
+    final fem = screenWidth / baseWidth;
+
     return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back),
-          onPressed: () {
-            Navigator.pop(context);
-          },
-        ),
-      ),
-      body: Container(
-        constraints: const BoxConstraints.expand(),
-        decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('assets/images/background.jpg'),
-            fit: BoxFit.cover,
-          ),
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Stack(
-              alignment: Alignment.topCenter,
+      body: Stack(
+        children: [
+          Container(
+            constraints: const BoxConstraints.expand(),
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/background.jpg'),
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: Stack(
+              alignment: Alignment.center,
               children: [
-                Container(
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(20),
+                Positioned(
+                  top: 185 * fem,
+                  child: Align(
+                    child: SizedBox(
+                      width: 350 * fem,
+                      height: 450 * fem,
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: const Color.fromARGB(172, 255, 255, 255),
+                          borderRadius: BorderRadius.circular(20 * fem),
+                        ),
+                      ),
+                    ),
                   ),
-                  padding: const EdgeInsets.all(20),
-                  margin: const EdgeInsets.symmetric(horizontal: 20),
-                  child: Column(
+                ),
+                Positioned(
+                  top: 269 * fem,
+                  child: SizedBox(
+                    width: 291 * fem,
+                    height: 42 * fem,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          width: 291 * fem,
+                          height: 42 * fem,
+                          decoration: BoxDecoration(
+                            color: const Color.fromARGB(181, 255, 255, 255),
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(20 * fem),
+                            ),
+                          ),
+                          child: TextField(
+                            controller: _usernameController,
+                            decoration: InputDecoration(
+                              labelText: 'Nome de Usuário',
+                              hintText: 'Nome de Usuário',
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(20 * fem),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: 339 * fem,
+                  child: SizedBox(
+                    width: 291 * fem,
+                    height: 42 * fem,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          width: 291 * fem,
+                          height: 42 * fem,
+                          decoration: BoxDecoration(
+                            color: const Color.fromARGB(181, 255, 255, 255),
+                            borderRadius: BorderRadius.all(
+                              Radius.circular(20 * fem),
+                            ),
+                          ),
+                          child: TextField(
+                            controller: _passwordController,
+                            obscureText: true,
+                            decoration: InputDecoration(
+                              labelText: 'Senha',
+                              hintText: 'Senha',
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.all(
+                                  Radius.circular(20 * fem),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: 409 * fem,
+                  child: TextButton(
+                    onPressed: () {
+                      _loginWithEmailAndPassword();
+                    },
+                    child: Container(
+                      width: 188 * fem,
+                      height: 42 * fem,
+                      decoration: BoxDecoration(
+                        color: const Color.fromARGB(255, 5, 21, 163),
+                        borderRadius: BorderRadius.circular(20 * fem),
+                      ),
+                      child: const Center(
+                        child: Text(
+                          'Entrar',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: 500 * fem,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
                     children: [
-                      Image.asset(
-                        'assets/images/logo_image.png',
-                        width: 250,
-                      ),
-                      const SizedBox(height: 10),
-                      const TextField(
-                        decoration: InputDecoration(
-                          labelText: 'Nome de Usuário',
-                          hintText: 'Nome de Usuário',
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.all(
-                              Radius.circular(20),
-                            ),
-                          ),
-                        ),
-                      ),
-                      const SizedBox(height: 20),
-                      const TextField(
-                        decoration: InputDecoration(
-                          labelText: 'Senha',
-                          hintText: 'Senha',
-                          border: OutlineInputBorder(
-                            borderRadius: BorderRadius.all(
-                              Radius.circular(20),
-                            ),
-                          ),
-                        ),
-                      ),
-                      const SizedBox(height: 20),
-                      SizedBox(
-                        // width: double.infinity,
-                        child: ElevatedButton(
-                          onPressed: () {
-                            Navigator.pushNamed(context, '/home');
-                          },
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor:
-                                const Color.fromARGB(255, 5, 21, 163),
-                            padding: const EdgeInsets.symmetric(
-                              horizontal: 20,
-                              vertical: 15,
-                            ),
-                            textStyle: const TextStyle(
-                              fontSize: 20,
-                            ),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(20),
-                            ),
-                          ),
-                          child: const Text('Entrar'),
-                        ),
-                      ),
-                      const SizedBox(height: 20),
-                      ElevatedButton.icon(
-                        onPressed: () {
-                          // Ação de conectar com o Google
+                      GestureDetector(
+                        onTap: () {
+                          _loginWithGoogle(context);
                         },
-                        style: ElevatedButton.styleFrom(
-                          foregroundColor: Colors.black,
-                          backgroundColor: Colors.white,
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(20),
-                          ),
-                          padding: const EdgeInsets.symmetric(
-                            horizontal: 20,
-                            vertical: 15,
-                          ),
+                        child: Image.asset(
+                          'assets/images/google_logo.png',
+                          width: 30 * fem,
+                          height: 30 * fem,
                         ),
-                        icon: Image.asset(
-                          'assets/images/google_icon.png',
-                          width: 24,
-                        ),
-                        label: const Text('Conectar com o Google'),
                       ),
-                      const SizedBox(height: 20),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          const Text('Não tem uma conta?'),
-                          TextButton(
-                            onPressed: () {
-                              Navigator.pushNamed(context, '/');
-                            },
-                            child: const Text('Cadastre-se'),
-                          ),
-                        ],
+                      SizedBox(width: 30 * fem),
+                      Image.asset(
+                        'assets/images/facebook_logo.png',
+                        width: 30 * fem,
+                        height: 30 * fem,
                       ),
                     ],
                   ),
                 ),
+                Positioned(
+                  top: 560 * fem,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        'Não tem uma conta?',
+                        style: TextStyle(
+                          fontSize: 16,
+                          color: Color.fromARGB(255, 0, 0, 0),
+                        ),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          Navigator.pushNamed(context, '/');
+                        },
+                        child: const Text(
+                          'Registrar',
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Color.fromARGB(255, 2, 23, 218),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Positioned(
+                  top: 110 * fem,
+                  child: Align(
+                    child: SizedBox(
+                      width: 150 * fem,
+                      height: 150 * fem,
+                      child: Image.asset(
+                        'assets/images/logo_image.png',
+                      ),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: 40 * fem,
+                  left: 10 * fem,
+                  child: IconButton(
+                    onPressed: () {
+                      Navigator.pushNamed(context, '/');
+                    },
+                    icon: const Icon(
+                      Icons.arrow_back_ios,
+                      color: Color.fromARGB(255, 2, 23, 218),
+                    ),
+                  ),
+                ),
               ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
