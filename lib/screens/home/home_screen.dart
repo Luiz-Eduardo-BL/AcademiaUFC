8def044c36e8385ff13f0c36a6f3f1742a7a5fbb
Luiz-Eduardo-BL <limadudu34@gmail.com>
2023-06-17 19:49:45 -0300

    Tela inicial

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,37 +1,89 @@
-// import 'package:flutter/material.dart';
-// import '../../database/database_helper.dart';
+import 'package:flutter/material.dart';
 
-// class HomeScreen extends StatelessWidget {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(title: const Text('Home')),
-//       body: Center(
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: [
-//             FutureBuilder(
-//               future: DatabaseHelper.instance.getUsuarioLogado(),
-//               builder: (context, snapshot) {
-//                 if (snapshot.hasData) {
-//                   return Text(snapshot.data.toString());
-//                 } else if (snapshot.hasError) {
-//                   return Text('Erro ao buscar dados');
-//                 } else {
-//                   return const CircularProgressIndicator();
-//                 }
-//               },
-//             ),
-//             ElevatedButton(
-//               onPressed: () {
-//                 // Redireciona para a tela de login
-//                 Navigator.pushReplacementNamed(context, '/login');
-//               },
-//               child: const Text('Sair'),
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
+class HomeScreen extends StatefulWidget {
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    const baseWidth = 390.0;
+    final fem = screenWidth / baseWidth;
+
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            constraints: const BoxConstraints.expand(),
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/background.jpg'),
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: Stack(alignment: Alignment.center, children: [
+              Positioned(
+                top: 185 * fem,
+                child: Align(
+                  child: SizedBox(
+                    width: 350 * fem,
+                    height: 450 * fem,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: const Color.fromARGB(172, 255, 255, 255),
+                        borderRadius: BorderRadius.circular(20 * fem),
+                      ),
+                    ),
+                    //Texto (Completou outra materia?) e botao Adicionar materia em baixo
+                  ),
+                ),
+              ),
+              Positioned(
+                child: Align(
+                  alignment: Alignment.center,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text(
+                        'Completou outra materia?',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                          color: Color.fromARGB(255, 0, 0, 0),
+                        ),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          Navigator.pushNamed(context, '/add');
+                        },
+                        child: const Icon(
+                          Icons.add,
+                          color: Color.fromARGB(255, 0, 0, 0),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Positioned(
+                top: 40 * fem,
+                left: 10 * fem,
+                child: IconButton(
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/');
+                  },
+                  icon: const Icon(
+                    Icons.arrow_back_ios,
+                    color: Color.fromARGB(255, 2, 23, 218),
+                  ),
+                ),
+              ),
+            ]),
+          )
+        ],
+      ),
+    );
+  }
+}
